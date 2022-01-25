cc69624cabd61be2edf21dcd889ee600422c6b51
html-css-javascript-andalittlepython <98429637+html-css-javascript-andalittlepython@users.noreply.github.com>
2022-01-25 16:07:13 -0600

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-function fact() {
-    document.getElementById("wrong-button").style.display = "none";
-    document.getElementById("true").innerHTML = "Correct";
-}
-
-function wrong() {
-    document.getElementById("fact-button").style.display = "none";
-    document.getElementById("true").innerHTML = "Incorrect"
-}
